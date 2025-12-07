3a77177586f20bd344f1c2baad2041a913c9aed7
Kepler <havrylenkooleh01@gmail.com>
2025-12-07 11:22:24 +0200

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,47 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const modifiedArr = [];
+  const finalSolution = [];
+  const modifiedState = { ...state };
+
+  modifiedArr.push(modifiedState);
+
+  let lastElement = modifiedArr[modifiedArr.length - 1];
+
+  for (const prop of actions) {
+    if (prop.type === 'clear') {
+      lastElement = {};
+
+      const roundLog = { ...lastElement };
+
+      finalSolution.push(roundLog);
+    }
+
+    if (prop.type === 'addProperties') {
+      for (const key in prop.extraData) {
+        lastElement[key] = prop.extraData[key];
+      }
+
+      const roundLog = { ...lastElement };
+
+      finalSolution.push(roundLog);
+    }
+
+    if (prop.type === 'removeProperties') {
+      for (const key of prop.keysToRemove) {
+        delete lastElement[key];
+      }
+
+      const roundLog = { ...lastElement };
+
+      finalSolution.push(roundLog);
+    }
+  }
+
+  return finalSolution;
 }
 
 module.exports = transformStateWithClones;
